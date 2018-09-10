811ca1214293751ad66f97692f84f455bf14f169
Joe Huss <detain@interserver.net>
2018-09-10 02:51:20 -0400

    formatting updates

--- src/MyDbHandler/MyDbHandler.php
+++ src/MyDbHandler/MyDbHandler.php
@@ -15,7 +15,8 @@ use DateTime;
  * Class MyDbHandler
  * @package detain\MydbHandler
  */
-class MyDbHandler extends AbstractProcessingHandler {
+class MyDbHandler extends AbstractProcessingHandler
+{
 
 	/**
 	 * @var bool defines whether the MySQL connection is been initialized
@@ -75,11 +76,13 @@ class MyDbHandler extends AbstractProcessingHandler {
 	 * @param bool $bubble
 	 * @param string $dateFormat        Format the time should be stored in
 	 */
-	public function __construct(Db $db = null, $table, $additionalFields = array(), $level = Logger::DEBUG, $bubble = true, $dateFormat = 'U') {
-		if (!is_null($db))
+	public function __construct(Db $db = null, $table, $additionalFields = array(), $level = Logger::DEBUG, $bubble = true, $dateFormat = 'U')
+	{
+		if (!is_null($db)) {
 			$this->db = $db;
-		else
+		} else {
 			$this->db = get_module_db('default');
+		}
 		$this->table = $table;
 		$this->additionalFields = $additionalFields;
 		$this->dateFormat = $dateFormat;
@@ -89,7 +92,8 @@ class MyDbHandler extends AbstractProcessingHandler {
 	/**
 	 * Initializes this handler by creating the table if it not exists
 	 */
-	private function initialize() {
+	private function initialize()
+	{
 		/*
 		$this->db->query('CREATE TABLE IF NOT EXISTS `'.$this->table.'` (
 			id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -106,8 +110,9 @@ class MyDbHandler extends AbstractProcessingHandler {
 		//Read out actual columns
 		$actualFields = array();
 		$this->db->query('DESCRIBE `'.$this->table.'`', __LINE__, __FILE__);
-		while ($this->db->next_record(MYSQL_ASSOC))
+		while ($this->db->next_record(MYSQL_ASSOC)) {
 			$actualFields[] = $this->db->Record['Field'];
+		}
 
 		//Calculate changed entries
 		$removedColumns = array_diff(
@@ -118,21 +123,27 @@ class MyDbHandler extends AbstractProcessingHandler {
 		$addedColumns = array_diff($this->additionalFields, $actualFields);
 
 		//Remove columns
-		if (!empty($removedColumns))
-			foreach ($removedColumns as $c)
+		if (!empty($removedColumns)) {
+			foreach ($removedColumns as $c) {
 				$this->db->query('ALTER TABLE `'.$this->table.'` DROP `'.$c.'`;', __LINE__, __FILE__);
+			}
+		}
 
 		//Add columns
-		if (!empty($addedColumns))
-			foreach ($addedColumns as $c)
+		if (!empty($addedColumns)) {
+			foreach ($addedColumns as $c) {
 				$this->db->query('ALTER TABLE `'.$this->table.'` add `'.$c.'` TEXT NULL DEFAULT NULL;', __LINE__, __FILE__);
+			}
+		}
 
 		// If the dateFormat supplied doesn't match the existing format then change the
 		// time format to whatever was passed
 		$existingTimeFormat = $this->getExistingTimeFormat();
-		if ($existingTimeFormat !== false)
-			if ($this->dateFormat != $existingTimeFormat)
+		if ($existingTimeFormat !== false) {
+			if ($this->dateFormat != $existingTimeFormat) {
 				$this->updateTimeFormat($existingTimeFormat, $this->dateFormat);
+			}
+		}
 
 		// merge default and additional field to one array
 		$this->defaultFields = array_merge($this->defaultFields, $this->additionalFields);
@@ -143,7 +154,8 @@ class MyDbHandler extends AbstractProcessingHandler {
 	/**
 	 * Prepare the sql query depending on the fields that should be written to the database and runs it
 	 */
-	private function process($values) {
+	private function process($values)
+	{
 		/*if (class_exists('\MyAdmin\Orm\Log')) {
 			$log = new \MyAdmin\Orm\Log();
 			foreach ($this->fields as $key => $f) {
@@ -156,21 +168,23 @@ class MyDbHandler extends AbstractProcessingHandler {
 			$log->save();
 		} else {
 		*/
-			//Prepare query
-			$columns = [];
-			$fields  = [];
-			foreach ($this->fields as $key => $f) {
-				if ($f == 'id')
-					continue;
-				$columns[] = $f;
-				if (is_null($values[$f]))
-					$fields[] = 'NULL';
-				elseif ($f != 'level' && is_numeric($values[$f]))
-					$fields[] = $values[$f];
-				else
-					$fields[] = "'".$this->db->real_escape($values[$f])."'";
+		//Prepare query
+		$columns = [];
+		$fields  = [];
+		foreach ($this->fields as $key => $f) {
+			if ($f == 'id') {
+				continue;
 			}
-			$this->db->query('INSERT INTO `' . $this->table . '` (' . implode(', ', $columns) . ') VALUES (' . implode(', ', $fields) . ')', __LINE__, __FILE__);
+			$columns[] = $f;
+			if (is_null($values[$f])) {
+				$fields[] = 'NULL';
+			} elseif ($f != 'level' && is_numeric($values[$f])) {
+				$fields[] = $values[$f];
+			} else {
+				$fields[] = "'".$this->db->real_escape($values[$f])."'";
+			}
+		}
+		$this->db->query('INSERT INTO `' . $this->table . '` (' . implode(', ', $columns) . ') VALUES (' . implode(', ', $fields) . ')', __LINE__, __FILE__);
 		/*}*/
 	}
 
@@ -181,9 +195,11 @@ class MyDbHandler extends AbstractProcessingHandler {
 	 * @param  $record[]
 	 * @return void
 	 */
-	protected function write(array $record) {
-		if (!$this->initialized)
+	protected function write(array $record)
+	{
+		if (!$this->initialized) {
 			$this->initialize();
+		}
 
 		/**
 		 * reset $fields with default values
@@ -195,8 +211,9 @@ class MyDbHandler extends AbstractProcessingHandler {
 		 * getting added to $record['extra']
 		 * @see https://github.com/Seldaek/monolog/blob/master/doc/02-handlers-formatters-processors.md
 		 */
-		if (isset($record['extra']))
+		if (isset($record['extra'])) {
 			$record['context'] = array_merge($record['context'], $record['extra']);
+		}
 
 		//'context' contains the array
 		$contentArray = array_merge(array(
@@ -207,7 +224,7 @@ class MyDbHandler extends AbstractProcessingHandler {
 		), $record['context']);
 
 		// unset array keys that are passed put not defined to be stored, to prevent sql errors
-		foreach($contentArray as $key => $context) {
+		foreach ($contentArray as $key => $context) {
 			if (! in_array($key, $this->fields)) {
 				unset($contentArray[$key]);
 				unset($this->fields[array_search($key, $this->fields)]);
@@ -230,20 +247,21 @@ class MyDbHandler extends AbstractProcessingHandler {
 	 *
 	 * @return string
 	 */
-	private function getTimeColumnType() {
+	private function getTimeColumnType()
+	{
 		$format = $this->dateFormat;
 
 		if ($format == "U") {
 			return "INTEGER";
-		} else if ($format == "Y-m-d") {
+		} elseif ($format == "Y-m-d") {
 			return "DATE";
-		} else if ($format == "Y-m-d H:i:s") {
+		} elseif ($format == "Y-m-d H:i:s") {
 			return "DATETIME";
-		} else if ($format == "YmdHis") {
+		} elseif ($format == "YmdHis") {
 			return "TIMESTAMP";
-		} else if ($format == "H:i:s") {
+		} elseif ($format == "H:i:s") {
 			return "TIME";
-		} else if ($format == "Y") {
+		} elseif ($format == "Y") {
 			return "YEAR";
 		}
 
@@ -255,7 +273,8 @@ class MyDbHandler extends AbstractProcessingHandler {
 	 *
 	 * @return string|boolean
 	 */
-	private function getExistingTimeFormat() {
+	private function getExistingTimeFormat()
+	{
 		$existingTimeFormat = '';
 
 		// Get the existing data type
@@ -267,15 +286,15 @@ class MyDbHandler extends AbstractProcessingHandler {
 		// We can determine the format from the data type
 		if ($existingColumnType == 'int') {
 			$existingTimeFormat = 'U';
-		} else if ($existingColumnType == 'date') {
+		} elseif ($existingColumnType == 'date') {
 			$existingTimeFormat = 'Y-m-d';
-		} else if ($existingColumnType == 'datetime') {
+		} elseif ($existingColumnType == 'datetime') {
 			$existingTimeFormat = 'Y-m-d H:i:s';
-		} else if ($existingColumnType== 'timestamp') {
+		} elseif ($existingColumnType== 'timestamp') {
 			$existingTimeFormat = 'YmdHis';
-		} else if ($existingColumnType == 'time') {
+		} elseif ($existingColumnType == 'time') {
 			$existingTimeFormat = 'H:i:s';
-		} else if ($existingColumnType == 'year') {
+		} elseif ($existingColumnType == 'year') {
 			$existingTimeFormat = 'Y';
 		} else {
 			// Not sure what to do about custom formats...
@@ -292,7 +311,8 @@ class MyDbHandler extends AbstractProcessingHandler {
 	 * @param  string $newFormat The format to update to
 	 * @return null
 	 */
-	private function updateTimeFormat($oldFormat, $newFormat) {
+	private function updateTimeFormat($oldFormat, $newFormat)
+	{
 		// Get the existing times
 		$this->db->query("SELECT id, time FROM {$this->table}", __LINE__, __FILE__);
 		$existingRows = [];
@@ -305,7 +325,8 @@ class MyDbHandler extends AbstractProcessingHandler {
 		$this->db->query("UPDATE {$this->table} SET time = NULL", __LINE__, __FILE__);
 		$this->db->query("ALTER TABLE {$this->table} CHANGE time time " . $this->getTimeColumnType(), __LINE__, __FILE__);
 		// Re-apply the times in the new format
-		foreach ($existingRows as $row)
+		foreach ($existingRows as $row) {
 			$this->db->query("UPDATE {$this->table} SET time = '{$row['time']}' WHERE id = {$row['id']}", __LINE__, __FILE__);
+		}
 	}
 }
